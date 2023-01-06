73404e85b3529489abf0f2b73686193f95c6e1af
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-01-06 13:27:33 +0100

    a

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ $(window).on("load",function(){
 // -------------- BUILDING AND POPULATING TABLE --------------
 // Credits: Dennis Ivy
 
-$.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/1', function(data) {  
+$.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/2', function(data) {  
     // Build Table with data
     buildTable(data)
 });
@@ -60,6 +60,7 @@ function buildTable(data){
                 break;
         }
 
+
         var row = `<tr class="${data[i].CreatorsChoice}">
                     <td class="rating">${data[i].Rating}</td>
                     <td>${data[i].Name} <span class="${playerClass}">${data[i].Players}</span> <span class="${genreClass}">${data[i].Genre}</span></td>
@@ -96,7 +97,7 @@ $("th").on("click", function(){
         myArray = myArray.sort((a,b) => a[column] < b[column] ? 1 : -1)
     }
     
-    $.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/1', function(data) {  
+    $.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/2', function(data) {  
         // Build Table with data
         buildTable(data)
     });
@@ -105,14 +106,14 @@ $("th").on("click", function(){
 
 
 // Updates text to amount of games rated
-$.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/1', function(data) { 
+$.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/2', function(data) { 
     // Determines how many games are listed
     let count = document.querySelector(".amount");    
     count.textContent = "Games rated: " + (data.length).toString();
 });
 
 // Determine average rating
-$.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/1', function(data) { 
+$.getJSON('https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/2', function(data) { 
     let avg = document.querySelector(".average");
     var sum = 0
     for(var i = 0; i < data.length; i++) {
