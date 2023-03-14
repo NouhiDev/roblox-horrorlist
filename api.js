4fd71e87574cfdb8438feba4c7cf12548373e43f
Nouhi <67205946+NouhiDev@users.noreply.github.com>
2023-03-14 11:50:03 +0100

    Mobile Support

--- api.js
+++ api.js
@@ -1,13 +1,32 @@
 $(window).on("load", async function () {
   //$(".loader").fadeOut(1000);
   //delay(1000).then(() => $(".content").style.opacity = 0);
-  $.getJSON(
-    "https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/2",
-    function (data) {
-      // Build Table with data
-      buildTable(data);
-    }
-  );
+  // Check if desktop or mobile
+  if (navigator.userAgent.match(/Android/i)
+         || navigator.userAgent.match(/webOS/i)
+         || navigator.userAgent.match(/iPhone/i)
+         || navigator.userAgent.match(/iPad/i)
+         || navigator.userAgent.match(/iPod/i)
+         || navigator.userAgent.match(/BlackBerry/i)
+         || navigator.userAgent.match(/Windows Phone/i)) {
+          // Mobile
+          $.getJSON(
+            "https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/2",
+            function (data) {
+              // Build Table with data
+              buildMobileTable(data);
+            }
+          );
+         } else {
+          // Desktop
+          $.getJSON(
+            "https://opensheet.elk.sh/16vH1l9tcKMEs8MATdjrp_Op-sMIL9-0jRQnBqFEthGo/2",
+            function (data) {
+              // Build Table with data
+              buildTable(data);
+            }
+          );
+         }
 });
 
 function buildTable(data) {
@@ -110,6 +129,90 @@ function buildTable(data) {
   });
 
   setUpTooltip();
+}
+
+function buildMobileTable(data) {
+  var table = document.getElementById("mobile-table-to-populate");
+  var tableDiv = document.getElementById("table-container");
+  tableDiv.style.display = "none";
+  tableDiv = document.getElementById("mobile-table-container");
+  tableDiv.style.display = "block";
+  var playerClass = "";
+  var genreClass = "";
+  table.innerHTML = "";
+
+  for (var i = 0; i < data.length; i++) {
+    // Determine Player Color
+    if (data[i].Players == "Multiplayer") {
+      playerClass = "tag blue-bg";
+    } else {
+      playerClass = "tag orange-bg";
+    }
+
+    // Determine Genre Color
+    switch (data[i].Genre) {
+      case "Chapters":
+        genreClass = "tag purple-bg";
+        break;
+      case "Story":
+        genreClass = "tag green-bg";
+        break;
+      case "Minigame":
+        genreClass = "tag yellow-bg";
+        break;
+      case "Misc":
+        genreClass = "tag dark-blue-bg";
+        break;
+      case "Myth":
+        genreClass = "tag pink-bg";
+        break;
+      case "Exploration":
+        genreClass = "tag dark-green-bg";
+        break;
+      case "Removed/Privated":
+        genreClass = "tag red-bg";
+        break;
+      case "Unfinished/Abandonend":
+        genreClass = "tag grey-bg";
+        break;
+      case "TBA":
+        genreClass = "tag grey-bg";
+        break;
+      case "Port":
+        genreClass = "tag dark-orange-bg";
+        break;
+      case "Survival Horror":
+        genreClass = "tag survival-horror";
+        break;
+      case "Abstract":
+        genreClass = "tag abstract";
+        break;
+      case "Fan Game":
+        genreClass = "tag fan-game";
+        break;
+      case "Classic Horror":
+        genreClass = "tag classic-horror";
+        break;
+    }
+
+    var row = `<tr>
+                        <td data-th="Placement">${i + 1}.</td>
+                        <td data="Icon"><img class="game-icon" src="${
+                          data[i].IconURL
+                        }"></td>
+                        <td data-th="Title" class="game-title">${
+                          data[i].Name
+                        }</span></td>
+                        <td data-th="Rating" class="align-left">${
+                          data[i].Rating
+                        }</td>
+                        </tr>`;
+    table.innerHTML += row;
+  }
+
+  $("#mobile-game-table").DataTable({
+    columnDefs: [{ orderable: false, targets: [1, 3] }],
+  });
 }
 
 function delay(time) {
