36307fefe8e97b67d022ae16c008aef7624e8819
nouhi <wolfgraphics.business@gmail.com>
2023-07-24 13:06:28 +0200

    game.html prev

---
+++ ndev-roblox-horrorlist-game.js
@@ -0,0 +1,6 @@
+function getInformationFromIndex() {
+    const number = localStorage.getItem("number");
+    const spreadSheetData = localStorage.getItem("spreadSheetData");
+    const gameData = localStorage.getItem("gameData");
+    const gameIconData = localStorage.getItem("gameIconData");
+}
