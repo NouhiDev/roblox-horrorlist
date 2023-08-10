d782d966b101992b77eaf53638faff239e297be8
nouhi <wolfgraphics.business@gmail.com>
2023-08-10 09:13:50 +0200

    post

--- scripts/ndev-roblox-horrorlist-community-ratings.js
+++ scripts/ndev-roblox-horrorlist-community-ratings.js
@@ -1,9 +1,43 @@
-// WIP
-fetch('https://ndevapi.com/ratings')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    const ratingForm = document.getElementById('ratingForm');
+
+    ratingForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const gameId = document.getElementById('gameId').value;
+        const rating = parseFloat(document.getElementById('rating').value);
+
+        const data = {
+            rating: rating
+        };
+
+        fetch(`/rate/${gameId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result.message);
+                // You can display a success message to the user or update the UI as needed
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle errors, display an error message, etc.
+            });
+    });
+});
+
+function getRatings() {
+    // WIP
+    fetch('https://ndevapi.com/ratings')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
