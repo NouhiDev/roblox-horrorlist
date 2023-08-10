df7e47153dc95e498ae95af99ef5a9c39d7712f9
nouhi <wolfgraphics.business@gmail.com>
2023-08-10 09:08:22 +0200

    Update ndev-roblox-horrorlist-community-ratings.js

--- scripts/ndev-roblox-horrorlist-community-ratings.js
+++ scripts/ndev-roblox-horrorlist-community-ratings.js
@@ -1,1 +1,8 @@
-alert("JS working!");
+fetch('https://your-api-domain.com/ratings')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
