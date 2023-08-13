022217ce9a0d9ee75e3fcceb360b1e83cc28f60d
nouhi <wolfgraphics.business@gmail.com>
2023-08-13 19:48:21 +0200

    Update ndev-roblox-horrorlist-filtered-search.js

--- scripts/ndev-roblox-horrorlist-filtered-search.js
+++ scripts/ndev-roblox-horrorlist-filtered-search.js
@@ -201,11 +201,11 @@ async function fetchAndDisplayGames(sortKey) {
             for (let i = 0; i < gameUIDS.length; i++) {
                 mobileTable.innerHTML = "";
                 try {
-                    let differenceToAverageRating = Math.abs((parseFloat(databaseData[i].ratings.rating) - averageRating)).toFixed(1);
+                    let differenceToAverageRating = (parseFloat(databaseData[i].ratings.rating) - averageRating).toFixed(1);
                     let spanHTML = "";
-                    if (differenceToAverageRating < 0) spanHTML = `<span style="color: red; font-size: 10px;">-${differenceToAverageRating}↓</span> `;
+                    if (differenceToAverageRating < 0) spanHTML = `<span style="color: red; font-size: 10px;">${differenceToAverageRating}↓</span> `;
                     else spanHTML = `<span style="color: green; font-size: 10px;">${differenceToAverageRating}↑</span> `;
-                    if (differenceToAverageRating == 0) spanHTML = `<span style="color: gray; font-size: 10px;">${differenceToAverageRating}-</span> `;
+                    if (differenceToAverageRating == 0) spanHTML = `<span style="color: gray; font-size: 10px;">${Math.abs((parseFloat(databaseData[i].ratings.rating) - averageRating)).toFixed(1)}-</span> `;
 
                     var row = ` <tr class="hover-reveal">
                       <td data-th="Placement">${i + 1}.</td>
