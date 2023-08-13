b40530be2e24e1358ddcb3c1b3bf336317a021f1
nouhi <wolfgraphics.business@gmail.com>
2023-08-13 17:22:48 +0200

    buttons

---
+++ scripts/ndev-roblox-horrorlist-buttons.js
@@ -0,0 +1,19 @@
+function backBtn() {
+    window.location.href = "https://robloxhorrorlist.com";
+}
+
+function githubButton() {
+    window.open("https://github.com/NouhiDev/roblox-horrorlist", "_blank");
+}
+
+function youtubeButton() {
+    window.open("https://www.youtube.com/@robloxhorrorlist", "_blank");
+}
+
+function discordButton() {
+    window.open("https://discord.gg/Zbxst3g4ts", "_blank");
+}
+
+function twitterButton() {
+    window.open("https://twitter.com/RBLXHorrorlist", "_blank");
+}
