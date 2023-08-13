58722307418dd04286ef90c61d66dedfe428cbb7
nouhi <wolfgraphics.business@gmail.com>
2023-08-13 03:59:43 +0200

    identifiers

--- scripts/ndev-roblox-horrorlist-main.js
+++ scripts/ndev-roblox-horrorlist-main.js
@@ -1,34 +1,47 @@
+//   _____   ____  ____  _      ______   __  _    _  ____  _____  _____   ____  _____  _      _____  _____ _______ 
+//  |  __ \ / __ \|  _ \| |    / __ \ \ / / | |  | |/ __ \|  __ \|  __ \ / __ \|  __ \| |    |_   _|/ ____|__   __|
+//  | |__) | |  | | |_) | |   | |  | \ V /  | |__| | |  | | |__) | |__) | |  | | |__) | |      | | | (___    | |   
+//  |  _  /| |  | |  _ <| |   | |  | |> <   |  __  | |  | |  _  /|  _  /| |  | |  _  /| |      | |  \___ \   | |   
+//  | | \ \| |__| | |_) | |___| |__| / . \  | |  | | |__| | | \ \| | \ \| |__| | | \ \| |____ _| |_ ____) |  | |   
+//  |_|  \_\\____/|____/|______\____/_/ \_\ |_|  |_|\____/|_|  \_\_|  \_\\____/|_|  \_\______|_____|_____/   |_|   
+
+// ID: NDev-ROBLOX-Horrorlist-MAIN
+
+//   For:
+//   robloxhorrorlist.com
+
+
 if (localStorage.getItem("firstEntry") === "true") {
     const popupContainer = document.getElementById("popupContainer");
     popupContainer.style.display = "none";
-  }
+}
 
-  function about() {
+function about() {
     window.open("pages/about.html", "_blank");
-  }
+}
 
-  function statistics() {
+function statistics() {
     window.open("pages/statistics.html", "_blank");
-  }
+}
 
-  function rating_scale() {
+function rating_scale() {
     window.open("pages/rating-overview.html", "_blank");
-  }
+}
 
-  function contributors() {
+function contributors() {
     window.open("pages/contributors.html", "_blank");
-  }
+}
 
-  function filtered() {
+function filtered() {
     window.location.href = "pages/filtered-search.html";
-  }
+}
 
-  function donate() {
+function donate() {
     window.open("https://www.buymeacoffee.com/robloxhorrorlist", "_blank");
-  }
+}
 
-  function firstEnter() {
+function firstEnter() {
     localStorage.setItem("firstEntry", "true");
     const popupContainer = document.getElementById("popupContainer");
     popupContainer.style.display = "none";
-  }
+}
