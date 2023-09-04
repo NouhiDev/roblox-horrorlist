a062ee66ef6080a035a53d209e51b0a608dc4e61
nouhi <wolfgraphics.business@gmail.com>
2023-09-04 15:01:33 +0200

    Update ndev-roblox-horrorlist-scraped.js

--- scripts/ndev-roblox-horrorlist-scraped.js
+++ scripts/ndev-roblox-horrorlist-scraped.js
@@ -1,3 +1,15 @@
+function formatLargeNumber(number) {
+    if (number >= 1e9) {
+      return (number / 1e9).toFixed(2) + 'B';
+    } else if (number >= 1e6) {
+      return (number / 1e6).toFixed(2) + 'M';
+    } else if (number >= 1e3) {
+      return (number / 1e3).toFixed(2) + 'K';
+    } else {
+      return number.toString();
+    }
+  }
+
 async function getScrapedGames() {
     const table = document.getElementById("table-to-populate");
     const databaseDataResponse = await fetch("https://ndevapi.com/scraped_horror_games");
@@ -12,8 +24,8 @@ async function getScrapedGames() {
          var row = ` <tr class="hover-reveal">
             <td data-th="Placement">${i + 1}.</td>
             <td data-th="UID" class="game-title">${databaseData[i].id}</td>
-            <td data-th="Name" class="align-left"><a href='https://www.roblox.com/games/${databaseData[i].placeId}'>${databaseData[i].name}</a></td>
-            <td data-th="Visits" class="align-left">${databaseData[i].visits}</td>
+            <td data-th="Name""><a href='https://www.roblox.com/games/${databaseData[i].placeId}'>${databaseData[i].name}</a></td>
+            <td data-th="Visits" class="align-left">${formatLargeNumber(databaseData[i].visits)}</td>
             </tr>`;
 
             const rowElement = document.createElement('tr');
